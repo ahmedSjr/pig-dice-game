82403956dc49ac8294f5523a9056b1670d503179
Ahemd Sirag <a7medsr.dev@gmail.com>
2021-12-05 09:03:52 +0200

    Modifying the game for production

--- script.js
+++ script.js
@@ -72,7 +72,7 @@ btnHold.addEventListener('click', function () {
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
     // 2.Check if player's score >= 100
-    if (scores[activePlayer] >= 15) {
+    if (scores[activePlayer] >= 100) {
       // Finish the game
       playing = false;
       diceEl.classList.add('hidden');
