2f3acfe4192b9ab8d213c4cab62034f89003dc30
Ahemd Sirag <a7medsr.dev@gmail.com>
2021-12-05 02:44:14 +0200

    Initialize the game interface value

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+// Select elements
+const score0 = document.querySelector('#score--0');
+const score1 = document.querySelector('#score--1');
+const diceEl = document.querySelector('.dice');
+
+score0.textContent = 0;
+score1.textContent = 0;
+diceEl.classList.add('hidden');
